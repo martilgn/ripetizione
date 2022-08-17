68d175a1aee953fdf1d46adccd08115218d7351f
Angelo Huang <xuanqiang.huang@studio.unibo.it>
2022-08-17 16:43:31 +0200

    fix primo

--- esercizio5.c
+++ esercizio5.c
@@ -3,14 +3,14 @@
 
 // TODO questa funzione è buggata
 // funziona solo per int da 0 a 9
-char *intToChar(int ch) {
+char intToChar(int ch) {
     // ragne giusto checks
     // int 1,2,34,... 9
     // 0x32 '0'
     // 0x33 '1'
     // 0x34 '2'
     // 0xXX '9'
-    return (char) ch + '0'; 
+    return (char) (ch + '0'); 
 }
 
 FILE *solve(FILE *file) {
@@ -19,16 +19,14 @@ FILE *solve(FILE *file) {
 
     FILE *toWrite = fopen("output.txt", "w");
     if (toWrite == NULL) {
-        printf("non ho potutto aprire in lettura");
+        printf("non ho potuto aprire in scrittura");
         exit(1); 
     }
 
     int count_chars = 0;
     int count_spaces = 0;
-    char ch = 'a';
+    char ch;
     while (fread(&ch, 1, 1, file) > 0) {
-        printf("%c ", &ch);
-
         if (ch == ' ') {
             char ch_count = intToChar(count_chars);
             fwrite(&ch_count, 1, 1, toWrite);
@@ -52,8 +50,10 @@ FILE *solve(FILE *file) {
 
 
 int main() {
-    FILE *f = fopen("./text.txt", "r"); 
-    // check  
+    FILE *f = fopen("text.txt", "r"); 
+    if (f == NULL) {
+        printf("Non ho potuto aprire il file in lettura"); 
+    }
     FILE *opened = solve(f);
 
     fclose(f);
--- esercizio7.c
+++ esercizio7.c
@@ -10,29 +10,27 @@ int somma(const char *str) {
     return sum;
 }
 
-
-    // f1 
-    // parola1 parola2 
-    // f2
-    // somma1 somma2 
+// Struttura dei file
+// f1 
+// parola1 parola2 
+// f2
+// somma1 somma2 
 int fun(FILE *f1, FILE *f2) {
     char buffer[1000];
     char buffer2[1000];
 
-    // se sono in numero differente, ritornare -1 
-    // se la somma non corrisponde ritornare 0 
-    // se invece tutto apposto ritornare il numero di parole letti 
+    // se sono in numero differente, ritornare -1
+    // se la somma non corrisponde ritornare 0
+    // se invece tutto apposto ritornare il numero di parole lette
     
     int primo = fscanf(f1, "%s", buffer);
     int secondo = fscanf(f2, "%s", buffer2);
     int parole_lette = 1; 
     while ((primo != EOF) && (secondo != EOF)) {
-
-        int isecondo = atoi(secondo); 
-        if (somma(primo) != isecondo) {
+        if (somma(buffer) != atoi(buffer2)) {
             return 0; 
         }
-
+        
         primo = fscanf(f1, "%s", buffer);
         secondo = fscanf(f2, "%s", buffer2);
         parole_lette += 1;
@@ -52,18 +50,20 @@ int fun(FILE *f1, FILE *f2) {
 }
 
 int main() {
-    const char *str = "hello"; 
-
-    int s = somma(str);
-    printf("%d\n", s);
-
-    FILE *f = fopen("./text.txt", "r");
+    FILE *f = fopen("text.txt", "r");
     if (f == NULL) {
         printf("exited");
         exit(1);
     }
-    fun(f, f);
+
+    FILE *f2 = fopen("text2.txt", "r");
+    if (f == NULL) {
+        printf("exited");
+        exit(1);
+    }
+    printf("%d\n", fun(f, f2));
     
     fclose(f);
+    fclose(f2);
     return 0; 
 }
